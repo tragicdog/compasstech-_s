957b266e6bcb548a9d3b5375fc04e266af194e89
Jon Lillie <jonlillie@gmail.com>
2015-06-28 09:56:45 -0700

    added horizontal rule (hr)

--- header.php
+++ header.php
@@ -32,6 +32,7 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'compasstech' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+                <hr />
+        </header><!-- #masthead -->
 
 	<div id="content" class="site-content">
